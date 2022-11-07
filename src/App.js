6b245a9c72b47abc4804685925d01084f2216ecf
Minhajmurad <minhajmurad421@gmail.com>
2022-11-07 22:26:53 +0600

    simple components created and flowbite,tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Booking from './Components/Booking/Booking';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Main from './Components/Main/Main';
+import Signup from './Components/Signup/Signup';
 
 function App() {
+  const router=createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>,
+        },
+        {
+          path:'/home',
+          element:<Home></Home>,
+        },
+        {
+          path:'/login',
+          element:<Login></Login>,
+        },
+        {
+          path:'/bookings',
+          element:<Booking></Booking>,
+        },
+        {
+          path:'/signup',
+          element:<Signup></Signup>,
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-     
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Components/Booking/Booking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Booking = () => {
+    return (
+        <div>
+            <h1>This is booking</h1>
+        </div>
+    );
+};
+
+export default Booking;
