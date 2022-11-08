998c97df050992ca25138e38b2e3b9ed12e80fef
Minhajmurad <minhajmurad421@gmail.com>
2022-11-09 02:49:45 +0600

    card added and data.json updated

--- src/Components/Home/Hotels/Hotel.js
+++ src/Components/Home/Hotels/Hotel.js
@@ -7,13 +7,15 @@ const Hotel = () => {
     const hotels = useLoaderData();
 
     return (
-        <div>
-            <h1 className='hotelsName  text-amber-600 font-extrabold text-4xl'>Hotels Nearby places:</h1>
-            {
+        <div >
+            <h1 className='hotelsName  text-amber-600 font-extrabold text-4xl '>Hotels Nearby places:</h1>
+           <div className='grid grid-cols-3 gap-12'>
+           {
                 hotels.map(hotel => <Hotelitems
                 key={hotel.id}
                 hotel={hotel}></Hotelitems>)
             }
+           </div>
 
         </div>
     );
--- src/Components/Home/Hotels/Hotelitems/Hotelitems.js
+++ src/Components/Home/Hotels/Hotelitems/Hotelitems.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
-const Hotelitems = ({hotel}) => {
+import './Hotelitem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+const Hotelitems = ({ hotel }) => {
     console.log(hotel);
+    const { img, price, rating, hotelName, place,description } = hotel;
     return (
         <div>
-            
+            <div data-aos="fade-up" data-aos-duration="3000" className="card card-compact w-96 bg-white shadow-2xl">
+                <figure><img className='card-img' src={img} alt="Shoes" /></figure>
+                <div className="card-body text-green-600">
+                    <h2 className="text-xl font-bold">{hotelName}</h2>
+                    <h2 className="text-lg font-semibold">{place}</h2>
+                  
+                       
+                       <h2 className="text-lg font-bold">Price : ${price}</h2>
+                       
+                         <p className='mt-1 '><FontAwesomeIcon icon={faStar } />   {rating}</p>
+                      
+                       <p>{description.length>200 ? description.slice(0,200)+'...' : description}</p>
+                    
+                    <div className="card-actions  justify-end">
+                        <button className="btn btn-primary">See details</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
